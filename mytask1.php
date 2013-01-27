abf74ef1a0878d7142da395689ae317d930122a5
Ardy Dedase <ardy@Ardys-MacBook-Pro-2.local>
2013-01-27 23:48:50 +0800

    Settled down with this solution using ternary operators.

--- mytask1.php
+++ mytask1.php
@@ -2,8 +2,50 @@
 <?php
 class MyTask1
 {
+	private $start;
+	private $end;
 
+	const FIZZ = 'Fizz';
+	const BUZZ = 'Buzz';
+
+	public function __construct($start, $end)
+	{
+		$this->start = $start;
+		$this->end = $end;
+	}
+
+	/**
+	 * "Fizz" if a multiple of 3
+	 * "Buzz" if a multiple of 5
+	 *  The integer itself otherwise.
+	 *
+	 * @return Array
+	 */
+	public function generateOutput()
+	{
+		for($i = $this->start; $i <= $this->end; $i++) {
+
+			$str = (($i % 3) == 0) ? self::FIZZ : '';
+			$str .= (($i % 5) == 0) ? self::BUZZ : '';
+
+			$minus_1 = $i - 1;
+			$minus_2 = $i - 2;
+
+			$output_array[] = ($str) ? $str : $i;
+		}
+
+		return $output_array;
+	}
 }
+
+$args = getopt('r:');
+$range = explode(',', $args['r']);
+
+$task = new MyTask1($range[0], $range[1]);
+$output = $task->generateOutput();
+
+echo implode(' ' ,$output);
+echo PHP_EOL;
 
 /**
  * End of file mytask1.php
